505553db7711163ae592017a0a40f9568fde7c2b
snegorania <ek.smuraga@gmail.com>
2022-09-08 11:02:45 +0300

    add lab2

---
+++ lab2/lab2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    class Class1
+    {
+        List<String> names = new List<String>();
+        static void zagz(String name) {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if ((int)name[i] < 65 || ((int)name[i] > 90 && (int)name[i] < 97) || (int)name[i] > 122)
+                {
+                    throw new 
+                }
+            }
+
+        }
+    }
+}
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = (int)'A';
+             MessageBox.Show((string)a);
+        }
+    }
+}
