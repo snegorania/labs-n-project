35299af1f71a630d30fbe55b2cb347699a83a129
snegorania <ek.smuraga@gmail.com>
2022-09-08 10:55:00 +0300

    add lab1

---
+++ lab1!/lab1!/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1_
+{
+    class Class1
+    {
+        public void kljafs()
+        {
+            Console.WriteLine("Здравствуйте!");
+        }
+    }
+}
---
+++ lab1!/lab1!/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+
+namespace lab1_
+{
+    class Program : Class1
+    {
+        public static int Main(string[] args)
+        {
+            lab1_.Class1 jgrgp = new lab1_.Class1();
+            jgrgp.kljafs();
+            MessageBox.Show("Эй...");
+            return 0;
+        }
+    }
+
+    
+
+}
