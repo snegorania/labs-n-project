e5bed11547b693af5ed5eacaacb6af02c807656c
GASArrow <realgasarrow@gmail.com>
2022-10-05 18:38:41 +0300

    Add constructor and destructor

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("dgfgsgs");
+        }
+    }
+}
---
+++ lab4/lab4/Stanki.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace lab4
+{
+    class Stanki
+    {
+        public string Name { get; }
+        public int Productivity { get; }
+        public int Size { get; }
+        public int LifeTime { get; }
+        public Stanki(string name, int productivity, int size, int lifeTime)
+        {
+            Name = name;
+            Productivity = productivity;
+            Size = size;
+            LifeTime = lifeTime;
+            Console.WriteLine("You have bought " + name);
+        }
+
+        ~Stanki()
+        {
+            Console.WriteLine("You have thrown out " + this.Name);
+        }
+    }
+}
