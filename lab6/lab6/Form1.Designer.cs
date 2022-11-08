390c71703022f9f461d390257995fc48238bc77e
GASArrow <realgasarrow@gmail.com>
2022-11-08 19:28:45 +0300

    Need to repair Change and Delete buttons

--- lab6/lab6/Form1.Designer.cs
+++ lab6/lab6/Form1.Designer.cs
@@ -32,15 +32,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.PetName = new System.Windows.Forms.TextBox();
-            this.PetType = new System.Windows.Forms.TextBox();
             this.PetStatus = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.PersonSpecialisation = new System.Windows.Forms.TextBox();
+            this.PersonAge = new System.Windows.Forms.TextBox();
+            this.PersonName = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -49,11 +51,11 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.PersonName = new System.Windows.Forms.TextBox();
-            this.PersonAge = new System.Windows.Forms.TextBox();
-            this.PersonSpecialisation = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.PetVolunteer = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -87,8 +89,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 36);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Rename";
+            this.button2.Text = "Change";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // PetName
             // 
@@ -97,16 +100,9 @@
             this.PetName.Size = new System.Drawing.Size(105, 22);
             this.PetName.TabIndex = 3;
             // 
-            // PetType
-            // 
-            this.PetType.Location = new System.Drawing.Point(143, 338);
-            this.PetType.Name = "PetType";
-            this.PetType.Size = new System.Drawing.Size(100, 22);
-            this.PetType.TabIndex = 4;
-            // 
             // PetStatus
             // 
-            this.PetStatus.Location = new System.Drawing.Point(267, 338);
+            this.PetStatus.Location = new System.Drawing.Point(123, 338);
             this.PetStatus.Name = "PetStatus";
             this.PetStatus.Size = new System.Drawing.Size(100, 22);
             this.PetStatus.TabIndex = 5;
@@ -115,27 +111,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.label1.Location = new System.Drawing.Point(9, 308);
+            this.label1.Location = new System.Drawing.Point(8, 317);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 18);
             this.label1.TabIndex = 6;
             this.label1.Text = "Name";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.label2.Location = new System.Drawing.Point(140, 308);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 18);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Type";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.label3.Location = new System.Drawing.Point(264, 308);
+            this.label3.Location = new System.Drawing.Point(120, 317);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 18);
             this.label3.TabIndex = 8;
@@ -149,6 +135,7 @@
             this.button3.TabIndex = 9;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // tabControl1
             // 
@@ -162,16 +149,16 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.PetVolunteer);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.comboBox1);
             this.tabPage1.Controls.Add(this.label8);
             this.tabPage1.Controls.Add(this.dataGridView1);
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.button3);
             this.tabPage1.Controls.Add(this.PetStatus);
-            this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.PetType);
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.PetName);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
@@ -182,8 +169,19 @@
             this.tabPage1.Text = "Animals";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(226, 317);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Volunteer";
+            // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.textBox1);
             this.tabPage2.Controls.Add(this.PersonSpecialisation);
             this.tabPage2.Controls.Add(this.PersonAge);
             this.tabPage2.Controls.Add(this.PersonName);
@@ -203,6 +201,27 @@
             this.tabPage2.Text = "People";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // PersonSpecialisation
+            // 
+            this.PersonSpecialisation.Location = new System.Drawing.Point(247, 312);
+            this.PersonSpecialisation.Name = "PersonSpecialisation";
+            this.PersonSpecialisation.Size = new System.Drawing.Size(100, 22);
+            this.PersonSpecialisation.TabIndex = 10;
+            // 
+            // PersonAge
+            // 
+            this.PersonAge.Location = new System.Drawing.Point(128, 312);
+            this.PersonAge.Name = "PersonAge";
+            this.PersonAge.Size = new System.Drawing.Size(100, 22);
+            this.PersonAge.TabIndex = 9;
+            // 
+            // PersonName
+            // 
+            this.PersonName.Location = new System.Drawing.Point(13, 312);
+            this.PersonName.Name = "PersonName";
+            this.PersonName.Size = new System.Drawing.Size(100, 22);
+            this.PersonName.TabIndex = 8;
+            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -219,7 +238,7 @@
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(99, 18);
             this.label6.TabIndex = 6;
-            this.label6.Text = "Specialisation";
+            this.label6.Text = "Specialization";
             // 
             // label5
             // 
@@ -249,6 +268,7 @@
             this.button6.TabIndex = 3;
             this.button6.Text = "Change";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
@@ -258,6 +278,7 @@
             this.button5.TabIndex = 2;
             this.button5.Text = "Delete";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -267,6 +288,7 @@
             this.button4.TabIndex = 1;
             this.button4.Text = "Add";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // dataGridView2
             // 
@@ -278,42 +300,47 @@
             this.dataGridView2.Size = new System.Drawing.Size(736, 268);
             this.dataGridView2.TabIndex = 0;
             // 
-            // PersonName
+            // textBox1
             // 
-            this.PersonName.Location = new System.Drawing.Point(13, 312);
-            this.PersonName.Name = "PersonName";
-            this.PersonName.Size = new System.Drawing.Size(100, 22);
-            this.PersonName.TabIndex = 8;
+            this.textBox1.Location = new System.Drawing.Point(13, 393);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 11;
             // 
-            // PersonAge
+            // label9
             // 
-            this.PersonAge.Location = new System.Drawing.Point(128, 312);
-            this.PersonAge.Name = "PersonAge";
-            this.PersonAge.Size = new System.Drawing.Size(100, 22);
-            this.PersonAge.TabIndex = 9;
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label9.Location = new System.Drawing.Point(10, 372);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(123, 18);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Change/Delete ID";
             // 
-            // PersonSpecialisation
+            // comboBox1
             // 
-            this.PersonSpecialisation.Location = new System.Drawing.Point(247, 312);
-            this.PersonSpecialisation.Name = "PersonSpecialisation";
-            this.PersonSpecialisation.Size = new System.Drawing.Size(100, 22);
-            this.PersonSpecialisation.TabIndex = 10;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(229, 338);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(515, 24);
+            this.comboBox1.TabIndex = 12;
             // 
-            // label8
+            // label2
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(400, 308);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(64, 16);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Volunteer";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(9, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Change/Delete ID";
             // 
-            // PetVolunteer
+            // textBox2
             // 
-            this.PetVolunteer.Location = new System.Drawing.Point(403, 338);
-            this.PetVolunteer.Name = "PetVolunteer";
-            this.PetVolunteer.Size = new System.Drawing.Size(100, 22);
-            this.PetVolunteer.TabIndex = 11;
+            this.textBox2.Location = new System.Drawing.Point(12, 398);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 13;
             // 
             // Form1
             // 
@@ -341,10 +368,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox PetName;
-        private System.Windows.Forms.TextBox PetType;
         private System.Windows.Forms.TextBox PetStatus;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TabControl tabControl1;
@@ -361,8 +386,12 @@
         private System.Windows.Forms.TextBox PersonSpecialisation;
         private System.Windows.Forms.TextBox PersonAge;
         private System.Windows.Forms.TextBox PersonName;
-        private System.Windows.Forms.TextBox PetVolunteer;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- lab6/lab6/Form1.cs
+++ lab6/lab6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace lab6
 {
@@ -19,18 +20,129 @@ namespace lab6
             PetShelter.addColums();
             People.AddCollumns();
             dataGridView1.DataSource = ds.Tables["Pets"];
-            dataGridView1.Update();
             dataGridView2.DataSource = ds.Tables["People"];
-            dataGridView2.Update();
+            comboBox1.Items.Add("All");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            DataRow row = PetShelter.pets.NewRow();
+            row["Name"] = PetName.Text;
+            if (comboBox1.SelectedIndex > 0)
+            {
+                row["Type"] = People.people.Rows[comboBox1.SelectedIndex - 1]["Specialization"];
+                People.people.Rows[comboBox1.SelectedIndex - 1]["WorkLoad"] = (int)People.people.Rows[comboBox1.SelectedIndex - 1]["WorkLoad"] + 1;
+            }
+            else
+            {
+                row["Type"] = "Unique";
+                foreach(DataRow item in People.people.Rows)
+                {
+                    item["WorkLoad"] = (int)item["WorkLoad"] + 1;
+                }
+            }
+            row["Status"] = PetStatus.Text;
+            row["Volunteer"] = comboBox1.SelectedIndex > 0 ? People.people.Rows[comboBox1.SelectedIndex - 1]["Name"] : "All";
+            PetShelter.pets.Rows.Add(row);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
             DataRow row = People.people.NewRow();
             row["Name"] = PersonName.Text;
             row["Age"] = PersonAge.Text;
-            row["Specialisation"] = PersonSpecialisation.Text;
+            row["Specialization"] = PersonSpecialisation.Text;
             People.people.Rows.Add(row);
+            comboBox1.Items.Add(row["Name"]);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DataRow row = People.people.Rows[Int32.Parse(textBox1.Text)];
+            row["Name"] = PersonName.Text;
+            row["Age"] = PersonAge.Text;
+            row["Specialization"] = PersonSpecialisation.Text;
+            comboBox1.Items[Int32.Parse(textBox1.Text) + 1] = row["Name"];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            People.people.Rows.RemoveAt(Int32.Parse(textBox1.Text));
+            comboBox1.Items.RemoveAt(Int32.Parse(textBox1.Text) + 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataRow row = PetShelter.pets.Rows[Int32.Parse(textBox2.Text)];
+            if (row["Volunteer"].Equals("All"))
+            {
+                foreach (DataRow item in People.people.Rows)
+                {
+                    if ((int)item["WorkLoad"] != 0)
+                    {
+                        item["WorkLoad"] = (int)item["WorkLoad"] - 1;
+                    }
+                }
+            }
+            else
+            {
+                foreach (DataRow item in People.people.Rows)
+                {
+                    if ((int)item["WorkLoad"] != 0 && item["Name"].Equals(row["Volunteer"]))
+                    {
+                        item["WorkLoad"] = (int)item["WorkLoad"] - 1;
+                    }
+                }
+            }
+            row["Name"] = PetName.Text;
+            if (comboBox1.SelectedIndex > 0)
+            {
+                row["Type"] = People.people.Rows[comboBox1.SelectedIndex - 1]["Specialization"];
+                People.people.Rows[comboBox1.SelectedIndex - 1]["WorkLoad"] = (int)People.people.Rows[comboBox1.SelectedIndex - 1]["WorkLoad"] + 1;
+            }
+            else
+            {
+                row["Type"] = "Unique";
+                foreach (DataRow item in People.people.Rows)
+                {
+                    item["WorkLoad"] = (int)item["WorkLoad"] + 1;
+                }
+            }
+            row["Status"] = PetStatus.Text;
+            row["Volunteer"] = comboBox1.SelectedIndex > 0 ? People.people.Rows[comboBox1.SelectedIndex - 1]["Name"] : "All";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataRow row = null;
+            foreach (DataRow thing in PetShelter.pets.Rows)
+            {
+                if ((int)thing["ID"] == Int32.Parse(textBox2.Text))
+                {
+                    row = thing;
+                    if (row["Volunteer"].Equals("All"))
+                    {
+                        foreach (DataRow item in People.people.Rows)
+                        {
+                            if ((int)item["WorkLoad"] != 0)
+                            {
+                                item["WorkLoad"] = (int)item["WorkLoad"] - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (DataRow item in People.people.Rows)
+                        {
+                            if ((int)item["WorkLoad"] != 0 && item["Name"].Equals(row["Volunteer"]))
+                            {
+                                item["WorkLoad"] = (int)item["WorkLoad"] - 1;
+                            }
+                        }
+                    }
+                }
+            }
+            PetShelter.pets.Rows.Remove(row);
         }
     }
 }
