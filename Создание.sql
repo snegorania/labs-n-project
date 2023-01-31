8ab0ad6d1e295f0059bc08b718245c0e5fcaffa8
GASArrow <81965088+GASArrow@users.noreply.github.com>
2023-01-31 15:54:01 +0300

    Add files via upload

---
+++ Создание.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Регионы (
+	Номер_региона int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	Название_региона varchar(162)
+);
+
+CREATE TABLE Датчики (
+	Номер_датчика int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	Номер_региона int,
+	Название_датчика varchar(100),
+	Пространственная_координата_датчика_широта double,
+	Пространственная_координата_датчика_долгота double,
+  FOREIGN KEY (Номер_региона) REFERENCES Регионы(Номер_региона)
+);
+
+CREATE TABLE Регистрация_показаний (
+	Номер_регистрации_показаний int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	Номер_датчика int,
+	Дата_регистрации_показаний date,
+	Время_регистрации_показаний time,
+	Значение_температуры double,
+  FOREIGN KEY (Номер_датчика) REFERENCES Датчики(Номер_датчика)
+);
---
+++ Создание_процедуры_мин_макс_температуры.sql
@@ -0,0 +1,5 @@
+﻿DELIMITER //
+CREATE PROCEDURE temp_min_max (IN temp_date1 date, IN temp_date2 date)
+BEGIN
+  SELECT MIN(Значение_температуры) AS min_temp, MAX(Значение_температуры) AS max_temp FROM регистрация_показаний WHERE Дата_регистрации_показаний BETWEEN temp_date1 AND temp_date2;
+END //
